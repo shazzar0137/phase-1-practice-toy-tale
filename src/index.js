e3a47d891b3b5e8d7312a805b79ccac13007f98b
Daniel Kipngetich <danielkipngetich10@gmail.com>
2025-03-28 18:01:24 +0300

    updates index.js

--- src/index.js
+++ src/index.js
@@ -13,3 +13,54 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+const toyForm = document.querySelector(".add-toy-form");
+toyForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = event.target.name.value;
+  const image = event.target.image.value;
+  const toy = {
+    name: name,
+    image: image,
+    likes: 0,
+  };
+  fetch("http://localhost:3000/toys", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(toy),
+  })
+    .then((response) => response.json())
+    .then((newToy) => {
+      const toyCollection = document.querySelector("#toy-collection");
+      const toyCard = document.createElement("div");
+      toyCard.className = "card";
+      toyCard.innerHTML = `
+        <h2>${newToy.name}</h2>
+        <img src="${newToy.image}" class="toy-avatar" />
+        <p>${newToy.likes} Likes </p>
+        <button class="like-btn">Like ❤️</button>
+      `;
+      toyCollection.appendChild(toyCard);
+      const likeButton = toyCard.querySelector(".like-btn");
+      likeButton.addEventListener("click", () => {
+        newToy.likes++;
+        fetch(`http://localhost:3000/toys/${newToy.id}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ likes: newToy.likes }),
+        })
+          .then((response) => response.json())
+          .then((updatedToy) => {
+            const likesParagraph = toyCard.querySelector("p");
+            likesParagraph.innerText = `${updatedToy.likes} Likes`;
+          });
+      });
+    }
+    );
+  toyForm.reset();
+}
+);
+const toyCollection = document.querySelector("#toy-collection");
